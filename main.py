267bb5f462e894ac4333d91f57fe8281888603b7
zombiglace <101476751+zombiglace@users.noreply.github.com>
2024-10-14 11:44:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,26 @@ import turtle
 #interraction
 
 #grille
-# Import the turtle module
+t = turtle.Turtle()
+
+size = 3
+
+cell_size = 30
+
+for i in range(size):
+    for j in range(size):
+        for _ in range(4):
+            t.forward(cell_size)
+            t.right(90)
+        t.forward(cell_size)
+
+    t.backward(cell_size * size)
+    t.right(90)
+    t.forward(cell_size)
+    t.left(90)
+
+turtle.done()
+
 
 # Create a new turtle screen and set its background color
 screen = turtle.Screen()
