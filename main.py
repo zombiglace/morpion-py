c71e0be409093feb874f2143dbcc024c91a22bba
zombiglace <101476751+zombiglace@users.noreply.github.com>
2024-10-14 11:45:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,20 +26,3 @@ for i in range(size):
 turtle.done()
 
 
-# Create a new turtle screen and set its background color
-screen = turtle.Screen()
-screen.bgcolor("white")
-
-# Create a new turtle object
-my_turtle = turtle.Turtle()
-
-# Draw a square
-for i in range(4):
-    my_turtle.forward(100)  # move forward by 100 units
-    my_turtle.right(90)     # turn right by 90 degrees
-
-# Keep the window open until the user closes it
-turtle.done()
-input("nom")
-#verification victoire
-
