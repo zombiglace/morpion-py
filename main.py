c9168e0da9877b5b85fa2435d1be522ec2385b2f
zombiglace <101476751+zombiglace@users.noreply.github.com>
2024-10-14 11:22:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,7 @@ from turtle import *
 screen.textinput("NIM", "Name of first player:")
 
 #variable cases
-a1= 
-a2=
-a3=
-a4=
-a5=
-a6=
-a7=
-a8=
-a9=
+
 #interraction
 
 #grille
@@ -32,4 +24,4 @@ for i in range(4):
 # Keep the window open until the user closes it
 turtle.done()
 #verification victoire
-if a1 == choix1 
+
