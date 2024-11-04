932bbaf675335f620cdb5e67ec3a69c24eeccce8
zombiglace <101476751+zombiglace@users.noreply.github.com>
2024-11-04 10:58:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,50 @@
 import turtle
 
-# Création de la fenêtre turtle
-win = turtle.Screen()
-win.setup(800, 800)
-
-# Création de la tortue
-t = turtle.Turtle()
-t.speed(3)
-
-# Définition de la taille de la grille
-size = 3
-cell_size = 100
-
 # Fonction pour dessiner la grille
 def draw_grid(size, cell_size):
     for i in range(size):
         for j in range(size):
             for _ in range(4):
-                t.forward(cell_size)
-                t.right(90)
-            t.forward(cell_size)
-        t.backward(cell_size * size)
-        t.right(90)
-        t.forward(cell_size)
-        t.left(90)
+                turtle.forward(cell_size)
+                turtle.right(90)
+            turtle.forward(cell_size)
+        turtle.backward(cell_size * size)
+        turtle.right(90)
+        turtle.forward(cell_size)
+        turtle.left(90)
 
-# Dessin de la grille
+
+def write_number_in_cell(x, y, number):
+    turtle.penup()
+    turtle.setposition(x + 25, y - 35)  
+    turtle.write(str(number), align="center", font=("Arial", 16, "normal"))
+    turtle.pendown()  
+
+
+size = 3 
+cell_size = 50 
+
+
+turtle.speed(0)  
+turtle.penup()
+turtle.setposition(-cell_size * size / 2, cell_size * size / 2) 
+turtle.pendown()
+
+
 draw_grid(size, cell_size)
 
-# Demande de la case
-case = win.textinput("Demande de case", "Entrez la case (ligne, colonne) : ")
+write_number_in_cell(-cell_size, cell_size, 1)  
+write_number_in_cell(0, cell_size, 2)        
+write_number_in_cell(cell_size, cell_size, 3)    
+write_number_in_cell(-cell_size, 0, 4)     
+write_number_in_cell(0, 0, 5)                 
+write_number_in_cell(cell_size, 0, 6)     
+write_number_in_cell(-cell_size, -cell_size, 7)    
+write_number_in_cell(0, -cell_size, 8)             
+write_number_in_cell(cell_size, -cell_size, 9)      
 
 
-if case:
-    try:
-        ligne, colonne = map(int, case.split(','))
-        if 1 <= ligne <= size and 1 <= colonne <= size:
-            # Dessin de la case sélectionnée
-            t.penup()
-            t.goto((colonne - 1) * cell_size, (ligne - 1) * cell_size)
-            t.pendown()
-            t.fillcolor('red')
-            t.begin_fill()
-            for _ in range(4):
-                t.forward(cell_size)
-                t.right(90)
-            t.end_fill()
-        else:
-            print("Case invalide")
-    except ValueError:
-        print("Erreur de saisie")
+#ingterface
 
-
-if case == 1:
-	print('caca')
-# Fermeture de la fenêtre turtle
+# Terminer le dessin
 turtle.done()
-
-
-
